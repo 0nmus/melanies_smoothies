5f5c221b8ee11ac90ba7709e38d6babebe6cdcf7
Ondrej Musil <91312612+0nmus@users.noreply.github.com>
2024-06-14 15:13:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col 
+from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -46,5 +47,3 @@ if ingredients_list:
             session.sql(my_insert_stmt).collect()
 
             st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
-
-import requests
